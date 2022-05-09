d324134e01633189d03c71efa54419c4685c76b1
Hoang NGUYEN <baauco@gmail.com>
2022-05-09 14:32:18 +0700

    🎨 Snapshot
    - Big purge
    - Move some input sanitizer
    - Better comments
    - Begin working on blocks

--- src/main/d96/checker/StaticCheck.py
+++ src/main/d96/checker/StaticCheck.py
@@ -13,7 +13,6 @@ from main.d96.utils.Utils import *
 from main.d96.utils.Visitor import *
 
 
-# Method type
 class MType:
     def __init__(self, partype, rettype):
         self.partype = partype  # This should not exist
@@ -90,6 +89,9 @@ class MetaMethod:
         self.check_redeclared_variable(name)
         self.variable[name] = MetaVariable(name, type, self.scope_nest, True)
 
+    def check_entrypoint(self):
+        return self.name == "main" and self.static
+
     def check_redeclared_variable(self, name):
         if name in self.variable.keys():
             raise Redeclared(Variable(), name)
@@ -108,7 +110,7 @@ class MetaClass:
         self.check_redeclared_attr(name)
         self.attr[name] = MetaAttribute(name, type, static, constant)
 
-    def add_method(self, name, partype, rettype=None):
+    def add_method(self, name, partype, rettype=None, static=False):
         self.check_redeclared_method(name, partype)
         self.method[name] = MetaMethod(name, partype, rettype)
 
@@ -123,21 +125,19 @@ class MetaClass:
         return self.method[name]
 
     def check_entrypoint(self):
-        return any(map(lambda method: method.name == "main", self.method))
+        return any(
+            [method.check_entrypoint()
+             for method in self.method.values()] if self.method else [False]
+        )
 
     def check_redeclared_attr(self, name):
-        if any(map(lambda attr: attr.name == name, self.attr)):
+        if name in self.attr.keys():
             raise Redeclared(Attribute(), name)
 
     def check_redeclared_method(self, name, partype: List[VarDecl]):
-        # TODO partype: List[VarDecl]
-        if any(
-            map(
-                lambda method: method.name == name and method.partype ==
-                partype, self.method
-            )
-        ):
-            raise Redeclared(Method(), self.name)
+        # TODO Test this partype comparison
+        if name in self.method.keys() and self.method[name].partype == partype:
+            raise Redeclared(Method(), name)
 
 
 class MetaProgram:
@@ -146,12 +146,13 @@ class MetaProgram:
 
     def add_class(self, name, super_cls=None):
         self.check_redeclared_class(name)
-        self.check_undeclared_class(super_cls)
+        if super_cls:
+            self.check_undeclared_class(super_cls)
         self.cls[name] = MetaClass(
             name, self.cls[super_cls] if super_cls else None
         )
 
-    def add_method(self, cls, name, partype, rettype, static=False):
+    def add_method(self, cls, name, partype, rettype=None, static=False):
         self.check_undeclared_class(cls)
         self.cls[cls].add_method(name, partype, rettype, static)
 
@@ -163,17 +164,19 @@ class MetaProgram:
         self.check_undeclared_class(name)
         return self.cls[name]
 
-    def check_undeclared_class(self, name):
-        if name and name not in self.cls.keys():
-            raise Undeclared(ClassType(name), name)
-
     def check_entrypoint(self):
-        if not any([cls.check_entrypoint() for cls in self.cls.values()]):
-            raise NoEntryPoint()
+        return any(
+            [cls.check_entrypoint()
+             for cls in self.cls.values()] if self.cls else [False]
+        )
 
     def check_redeclared_class(self, name):
         if name in self.cls.keys():
-            raise Redeclared(ClassType(name), name)
+            raise Redeclared(Class(), name)
+
+    def check_undeclared_class(self, name):
+        if name not in self.cls.keys():
+            raise Undeclared(Class(), name)
 
 
 class StaticChecker(BaseVisitor, Utils):
@@ -181,241 +184,73 @@ class StaticChecker(BaseVisitor, Utils):
         self.ast = ast
 
     def check(self):
-        # Used to be global_envi here, but no idea how it is used
         return self.visit(self.ast, None)
 
-    def visitProgram(self, ast, c):
-        # Dependency rejection
+    def visitProgram(self, ast, global_env=None):
         self.meta_program = MetaProgram()
 
         # Traverse all classes
-        [self.visit(x, c) for x in ast.decl]
+        [self.visit(x, global_env) for x in ast.decl]
 
         if not self.meta_program.check_entrypoint():
             raise NoEntryPoint()
 
-    def visitClassDecl(self, ast, c):
-        # Load class into meta_program
+    def visitClassDecl(self, ast, c=None):
+        # Load classes into meta_program
         self.meta_program.add_class(
             ast.classname.name, ast.parentname.name if ast.parentname else None
         )
 
-        # Traverse all attributes and methods
+        # Make classes load attributes and methods
         [
             self.visit(x, self.meta_program.get_class(ast.classname.name))
             for x in ast.memlist
         ]
 
-    def visitClass_mem_decl(self, ast, c):
-        # TODO Remove
+    def visitMethodDecl(self, ast, meta_cls: MetaClass):
+        static = type(ast.kind) is Static
+        meta_cls.add_method(ast.name.name, ast.param, None, static)
+        [
+            self.visit(
+                ast.body, (
+                    meta_cls,
+                    meta_cls.get_or_raise_undeclared_method(ast.name.name)
+                )
+            )
+        ]
+
+    def visitBlock(self, ast, c: tuple):
+        meta_class, meta_method = c
+        meta_method.enter_scope()
+        # TODO ast.inst is [None, None] for some reason
+        [self.visit(x, c) for x in ast.inst]
+        meta_method.exit_scope()
+
+    def visitAttributeDecl(self, ast, c):
         pass
 
-    def visitAttr_decl(self, ast, c):
-        # TODO
+    def visitAssign(self, ast, c):
         pass
 
-    def visitAttr_decl_wo_asg(self, ast, c):
-        # TODO
+    def visitIf(self, ast, c):
         pass
 
-    def visitAttr_decl_w_asg(self, ast, c):
-        # TODO
-        pass
-
-    def visitAttr_decl_rep(self, ast, c):
-        # TODO
-        pass
-
-    def visitAttr_nameList(self, ast, c):
-        # TODO
-        pass
-
-    def visitMethodDecl(self, ast, c):
-        # TODO
-        pass
-
-    def visitGeneric_name(self, ast, c):
-        # TODO
-        pass
-
-    def visitMethod_local_decl(self, ast, c):
-        # TODO
-        pass
-
-    def visitMethod_local_decl_wo_asg(self, ast, c):
-        # TODO
-        pass
-
-    def visitMethod_local_decl_w_asg(self, ast, c):
-        # TODO
-        pass
-
-    def visitMethod_local_decl_rep(self, ast, c):
-        # TODO
-        pass
-
-    def visitConsMethod(self, ast, c):
-        # TODO
-        pass
-
-    def visitDesMethod(self, ast, c):
-        # TODO
-        pass
-
-    def visitParamList(self, ast, c):
-        # TODO
-        pass
-
-    def visitParam(self, ast, c):
-        # TODO
-        pass
-
-    def visitLiteralList(self, ast, c):
-        # TODO
-        pass
-
-    def visitLiteral(self, ast, c):
-        # TODO
-        pass
-
-    def visitIntLiteral(self, ast, c):
-        return IntType()
-
-    def visitBooleanLiteral(self, ast, c):
-        return BoolType()
-
-    def visitStaticIdnameList(self, ast, c):
-        # TODO
-        pass
-
-    def visitIdnameList(self, ast, c):
-        # TODO
-        pass
-
-    def visitArrayLiteral(self, ast, c):
-        # TODO
-        pass
-
-    def visitAnyType(self, ast, c):
-        # TODO
-        pass
-
-    def visitExprList(self, ast, c):
-        # TODO
-        pass
-
-    def visitExpr(self, ast, c):
-        # TODO
-        pass
-
-    def visitExpr1(self, ast, c):
-        # TODO
-        pass
-
-    def visitExpr2(self, ast, c):
-        # TODO
-        pass
-
-    def visitExpr3(self, ast, c):
-        # TODO
-        pass
-
-    def visitExpr4(self, ast, c):
-        # TODO
-        pass
-
-    def visitExpr5(self, ast, c):
-        # TODO
-        pass
-
-    def visitExpr6(self, ast, c):
-        # TODO
-        pass
-
-    def visitExpr7(self, ast, c):
-        # TODO
-        pass
-
-    def visitExpr8(self, ast, c):
-        # TODO
-        pass
-
-    def visitExpr9(self, ast, c):
-        # TODO
-        pass
-
-    def visitExpr10(self, ast, c):
-        # TODO
-        pass
-
-    def visitOperand(self, ast, c):
-        # TODO
-        pass
-
-    def visitNewObjectExpr(self, ast, c):
-        # TODO
-        pass
-
-    def visitAssignStmt(self, ast, c):
-        # TODO
-        pass
-
-    def visitAssignLhs(self, ast, c):
-        # TODO
-        pass
-
-    def visitAssignRhs(self, ast, c):
-        # TODO Remove
-        pass
-
-    def visitFlowStmt(self, ast, c):
-        # TODO
-        pass
-
-    def visitIfStmt(self, ast, c):
-        # TODO
-        pass
-
-    def visitElseifStmt(self, ast, c):
-        # TODO
-        pass
-
-    def visitElseStmt(self, ast, c):
-        # TODO
-        pass
-
-    def visitForStmt(self, ast, c):
-        # TODO
-        pass
-
-    def visitForRange(self, ast, c):
-        # TODO
+    def visitFor(self, ast, c):
         pass
 
     def visitBreakStmt(self, ast, c):
+        pass
         is_in_loop = c[1]
         if not is_in_loop:
             raise MustInLoop('Break')
         return (ast, Break())
 
     def visitContinueStmt(self, ast, c):
+        pass
         is_in_loop = c[1]
         if not is_in_loop:
             raise MustInLoop('Continue')
         return (ast, Continue())
 
-    def visitReturnStmt(self, ast, c):
-        # TODO
-        pass
-
-    def visitAnyStmt(self, ast, c):
-        # TODO Remove
-        pass
-
-    def visitCallStmt(self, ast, c):
-        # TODO
-        pass
-
-    def visitBlockStmt(self, ast, c):
-        # TODO
+    def visitReturn(self, ast, c):
         pass
