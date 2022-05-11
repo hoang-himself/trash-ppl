4d5e970759545f07507e3f86ab51b9c6d61c208f
Hoang NGUYEN <baauco@gmail.com>
2022-05-11 11:35:30 +0700

    ✅ Add test from D96 specs

--- src/test/CheckerSuite.py
+++ src/test/CheckerSuite.py
@@ -104,70 +104,54 @@ class CheckerSuite(unittest.TestCase):
         expect = """Type Mismatch In Constant Declaration: ConstDecl(Id(myVar),IntType,FloatLit(1.2))"""
         self.assertTrue(TestChecker.test(input, expect, 5))
 
-    # def test_undeclared_function(self):
-    #     """Simple program: int main() {} """
-    #     input = """int main() {foo();}"""
-    #     expect = "Undeclared Function: foo"
-    #     self.assertTrue(TestChecker.test(input, expect, 400))
+    def test_spec(self):
+        input = """
+            Class Shape {
+                Val $numOfShape: Int = 0;
+                Val immutableAttribute: Int = 0;
+                $getNumOfShape() {
+                    Return Shape::$numOfShape;
+                }
+            }
+            Class Rectangle: Shape {
+                Var length, width: Int;
+                getArea() {
+                    Return Self.length * Self.width;
+                }
+            }
+            Class Program {
+                main() {
+                    Out.printInt(Shape::$numOfShape);
+                }
+            }
+        """
+        expect = """Undeclared Class: Out"""
+        self.assertTrue(TestChecker.test(input, expect, 6))
 
-    # def test_diff_numofparam_stmt(self):
-    #     """More complex program"""
-    #     input = """int main () {
-    #         putIntLn();
-    #     }"""
-    #     expect = "Type Mismatch In Statement: CallExpr(Id(putIntLn),List())"
-    #     self.assertTrue(TestChecker.test(input, expect, 401))
+    def test_1(self):
+        input = """
+        Class Program {}
+        Class Program {}
+        """
+        expect = """Redeclared Class: Program"""
+        self.assertTrue(TestChecker.test(input, expect, 101))
 
-    # def test_diff_numofparam_expr(self):
-    #     """More complex program"""
-    #     input = """int main () {
-    #         putIntLn(getInt(4));
-    #     }"""
-    #     expect = "Type Mismatch In Expression: CallExpr(Id(getInt),List(IntLiteral(4)))"
-    #     self.assertTrue(TestChecker.test(input, expect, 402))
+    def test_2(self):
+        input = """
+        Class woa: error {}
+        Class Program {
+            main() {}
+        }
+        """
+        expect = """Undeclared Class: error"""
+        self.assertTrue(TestChecker.test(input, expect, 102))
 
-    # def test_undeclared_function_use_ast(self):
-    #     """Simple program: int main() {} """
-    #     input = Program(
-    #         [
-    #             FuncDecl(
-    #                 Id("main"), [], IntType(),
-    #                 Block([], [CallExpr(Id("foo"), [])])
-    #             )
-    #         ]
-    #     )
-    #     expect = "Undeclared Function: foo"
-    #     self.assertTrue(TestChecker.test(input, expect, 403))
-
-    # def test_diff_numofparam_expr_use_ast(self):
-    #     """More complex program"""
-    #     input = Program(
-    #         [
-    #             FuncDecl(
-    #                 Id("main"), [], IntType(),
-    #                 Block(
-    #                     [], [
-    #                         CallExpr(
-    #                             Id("putIntLn"),
-    #                             [CallExpr(Id("getInt"), [IntLiteral(4)])]
-    #                         )
-    #                     ]
-    #                 )
-    #             )
-    #         ]
-    #     )
-    #     expect = "Type Mismatch In Expression: CallExpr(Id(getInt),List(IntLiteral(4)))"
-    #     self.assertTrue(TestChecker.test(input, expect, 404))
-
-    # def test_diff_numofparam_stmt_use_ast(self):
-    #     """More complex program"""
-    #     input = Program(
-    #         [
-    #             FuncDecl(
-    #                 Id("main"), [], IntType(),
-    #                 Block([], [CallExpr(Id("putIntLn"), [])])
-    #             )
-    #         ]
-    #     )
-    #     expect = "Type Mismatch In Statement: CallExpr(Id(putIntLn),List())"
-    #     self.assertTrue(TestChecker.test(input, expect, 405))
+    def test_3(self):
+        input = """
+            Class Shape {
+                sample(num: Int) {}
+                sample(num: Int) {}
+            }
+        """
+        expect = """Redeclared Method: sample"""
+        self.assertTrue(TestChecker.test(input, expect, 103))
